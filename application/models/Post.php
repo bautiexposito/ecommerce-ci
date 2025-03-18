f5c24077054466cb330775429a94029fc2526f27
bautiexposito <bautistaexposito@outlook.com>
2025-03-18 10:29:27 -0300

    modelos Post, Category, y metodos crud

---
+++ application/models/Post.php
@@ -0,0 +1,6 @@
+<?php
+
+class Post extends CI_Model {
+    public $table = "posts";
+    public $table_id = "post_id";
+}
--- system/core/Model.php
+++ system/core/Model.php
@@ -74,4 +74,35 @@ class CI_Model {
 		return get_instance()->$key;
 	}
 
+	function findAll() {
+		$this->db->select("*");
+		$this->db->from($this->table);
+
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function findByID($id) {
+		$this->db->select("*");
+		$this->db->from($this->table);
+		$this->db->where($this->table_id, $id);
+		
+		$query = $this->db->get();
+		return $query->row();
+	}
+
+	function insert ($data) {
+		$this->db->insert($this->table, $data);
+		return $this->db->insert_id();
+	}
+
+	function update($id) {
+		$this->db->where($this->table_id, $id);
+		$this->db->update($this->$table);
+	}
+
+	function delete($id) {
+		$this->db->where($this->table_id, $id);
+		$this->db->delete($this->$table);
+	}
 }
