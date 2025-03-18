27134c37905a5b3a329aa61c28c16038c4bacf3b
bautiexposito <bautistaexposito@outlook.com>
2025-03-18 11:22:44 -0300

    definicion de variables globales, e instalacion de Bootstrap y jQuery

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,10 +4,14 @@ class Admin extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this ->load->helper("url");
     }
 
     public function index(){
         $this->load->view('admin/admin_home');
     }
 
+    public function post_list() {
+        $this->load->view("admin/post_list");
+    }
 }
